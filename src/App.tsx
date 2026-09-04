90ba6e05d4bcb23923dcddbacb2f1bf4d55360a1
yunseonam <sjw070283828@gmail.com>
2026-09-04 16:45:20 +0900

    Analytics 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import RouterSetting from "./routes/routerList"
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
+    <>
       <BrowserRouter>
         <RouterSetting/>
       </BrowserRouter>
+      <Analytics/>
+    </>
   );
 }
 
